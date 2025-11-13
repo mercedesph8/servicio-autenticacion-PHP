6bb19a1f8853a8fd5a2d9cf639683634abeb4d8e
Mercedes <2000mariamercedes@gmail.com>
2025-11-14 00:09:54 +0100

    permisos y css

---
+++ permisos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sin Permisos</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+    
+    <div class="contenedor">
+        <div class="caja-permisos">
+            <h1>Acceso Denegado</h1>
+            <p>Debes iniciar sesión para poder acceder a esta página </p>
+            
+            <a href="login.php" class="boton-volver">Ir al Login</a>
+        </div>
+    </div>
+    
+</body>
+</html>
